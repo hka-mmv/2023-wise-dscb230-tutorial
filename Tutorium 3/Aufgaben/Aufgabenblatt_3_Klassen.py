e612b655105a8dc20bd038d10b79fbcae1b2b78d
Wladimir Schairer <schairer.wladimir@gmail.com>
2023-10-24 15:47:44 +0200

    Tutorium 3 Aufgaben

---
+++ Tutorium 3/Aufgaben/Aufgabenblatt_3_Klassen.py
@@ -0,0 +1,15 @@
+# Python: Eine Klasse, die durch eine Länge und Breite konstruiert wird und eine Methode,
+# die den Flächeninhalt eines Rechtecks berechnet
+
+# In der euklidischen Geometrie ist ein Rechteck ein Viereck mit vier rechten Winkeln.
+# Um den Flächeninhalt eines Rechtecks zu finden, multipliziert man die Länge mit der Breite.
+# Ein Rechteck mit vier gleich langen Seiten ist ein Quadrat.
+
+# Aufgabe:
+# Erstellen Sie eine Klasse namens "Rechteck".
+# Die Klasse sollte durch zwei Parameter, Länge und Breite, konstruiert werden.
+# Fügen Sie eine Methode hinzu, die den Flächeninhalt des Rechtecks berechnet.
+
+
+class Rechteck:
+    pass
