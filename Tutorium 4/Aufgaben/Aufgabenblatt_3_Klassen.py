8dea190655f10c54b19c8f41d2b00ab8e23a0bfa
Wladimir Schairer <schairer.wladimir@gmail.com>
2023-10-31 15:49:04 +0100

    Tutorium 4 Aufgaben

---
+++ Tutorium 4/Aufgaben/Aufgabenblatt_3_Klassen.py
@@ -0,0 +1,19 @@
+# Übungsblatt 3 für Python Klassen
+
+"""
+Ziel: In diesem Übungsblatt werdet Ihr eine Klasse in Python erstellen und Methoden innerhalb dieser Klasse definieren.
+"""
+
+# Übung: Klasse Kreis
+
+# Anweisung:
+# 1. Erstellen Sie eine Python-Klasse namens "Kreis", die durch einen Radius konstruiert wird.
+# 2. Definieren Sie zwei Methoden innerhalb dieser Klasse:
+#    - Eine Methode namens "flaeche", die die Fläche des Kreises berechnet.
+#    - Eine Methode namens "umfang", die den Umfang des Kreises berechnet.
+# 3. Erstellen Sie ein Objekt dieser Klasse und rufen Sie beide Methoden auf, um die Fläche und den Umfang des Kreises zu berechnen.
+# 4. Geben Sie die berechneten Werte aus.
+#
+# Ihr könnt für pi den Wert 3.14 oder im modul math könnt ihr math.pi verwenden.
+
+# TODO:
